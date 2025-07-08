aa7728a8ec0fb84a9674e58ccf877d28ba86f7a7
kelvyn2012 <kudufo@gmail.com>
2025-07-08 10:23:34 +0100

    updated my admin design

--- book_store/admin.py
+++ book_store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price")  # Show these columns
+    search_fields = ("name", "category")  # Search bar
+    list_filter = ("category",)  # Sidebar filter
+    ordering = ("-price",)  # Order by price descending
+
 
 # Register your models here.
+admin.site.register(Product, ProductAdmin)
